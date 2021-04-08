d53795e23e682a4393ab212d729e239eb7bc0e97
Felipe Finger <felipe.finger@outlook.com>
2021-04-08 10:39:10 -0300

    Adicionado url do servidor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.102:8080'
+    //baseURL: 'https://api-mita.herokuapp.com/'
+    baseURL: 'http://192.168.0.103:8080'
 })
 
 export default api;
